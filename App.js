07e5fb6d9a33a0b761a8679abb0542f6e362c94c
Sayuri Mizuguchi <adoravel_sansei@hotmail.com>
2020-01-27 23:07:38 -0500

    Removing initialization of lcoalize

--- App.js
+++ App.js
@@ -1,23 +1,7 @@
 import React from 'react';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
-import i18next from 'i18next';
-import { initReactI18next } from 'react-i18next';
 
 import { TextComponent } from './components/text.component';
-import { i18nextOptions } from './utils/localize.utils'
-
-const languageDetector = {
-  type: 'languageDetector',
-  async: true,
-  detect: cb => cb('en'),
-  init: () => {},
-  cacheUserLanguage: () => {},
-};
-
-i18next
-  .use(languageDetector)
-  .use(initReactI18next)
-  .init(i18nextOptions);
 
 const App = () => {
   return (
