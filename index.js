e5d6f05e0c35b70afc1c0c921ee2b56c391ffcd6
Sayuri Mizuguchi <adoravel_sansei@hotmail.com>
2020-01-27 23:07:59 -0500

    Adding initialization on indexjs

--- index.js
+++ index.js
@@ -2,8 +2,21 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
+import { initReactI18next } from 'react-i18next';
+import i18next from 'i18next';
+
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { languageDetector } from './utils/localize.utils'
+
+
+import { i18nextOptions } from './utils/localize.utils'
+
+i18next
+  .use(languageDetector)
+  .use(initReactI18next)
+  .init(i18nextOptions);
+
 
 AppRegistry.registerComponent(appName, () => App);
