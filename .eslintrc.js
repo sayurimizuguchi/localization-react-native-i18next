f4a459430d5cab7f8ae9d54459a0f7ba3a5ec8ab
Sayuri Mizuguchi <adoravel_sansei@hotmail.com>
2020-04-28 15:54:45 -0400

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,18 @@
 module.exports = {
   root: true,
 	extends: 'airbnb',
+	parser: "babel-eslint",
+	plugins: ["jsx-a11y", "import", "react"],
 	rules: {
 		"react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
 		"import/prefer-default-export": 0,
 		"no-use-before-define": 0,
+		'react/jsx-props-no-spreading': 0,
+		"arrow-body-style": ["error", "as-needed"],
+		"global-require": 0,
+		"no-multiple-empty-lines": [
+      "error",
+      { "max": 1, "maxBOF": 0, "maxEOF": 0 }
+    ],
 	}
 };
