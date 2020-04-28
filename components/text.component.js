4230da67796d379f63716b6f1214054ec8ae31a3
Sayuri Mizuguchi <adoravel_sansei@hotmail.com>
2020-04-28 15:54:59 -0400

    eslint text component

--- components/text.component.js
+++ components/text.component.js
@@ -4,16 +4,18 @@ import { Text } from 'react-native';
 import { translate } from '../utils/localize.utils';
 
 type Props = {
-	keyText: String,
-	interpolation: Object,
-	style: Object
+  keyText: String,
+  interpolation: Object,
+  style: Object
 };
 
-export const LocaleText = ({ keyText, interpolation = {}, style, ...rest }: Props) => {
-	let textContent = translate(keyText, interpolation).trim();
+export const LocaleText = ({
+  keyText, interpolation = {}, style, ...rest
+}: Props) => {
+  const textContent = translate(keyText, interpolation).trim();
   return (
-      <Text {...style} {...rest}>
-				{textContent}
-      </Text>
+    <Text {...style} {...rest}>
+      {textContent}
+    </Text>
   );
 };
