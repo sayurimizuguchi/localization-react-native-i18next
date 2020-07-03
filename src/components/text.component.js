36698c996fc00558e66868e7b44b00ce2302cab0
Sayuri Mizuguchi <adoravel_sansei@hotmail.com>
2020-07-03 15:54:05 -0400

    update path text component

---
+++ src/components/text.component.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { translate } from '../services/localization/localize.utils';
+
+type Props = {
+  keyText: String,
+  interpolation: Object,
+  style: Object
+};
+
+export const LocaleText = ({
+  keyText, interpolation = {}, style = {}, ...rest
+}: Props) => {
+  const textContent = translate(keyText, interpolation).trim();
+  return (
+    <Text style={{...style}} {...rest}>
+      {textContent}
+    </Text>
+  );
+};
